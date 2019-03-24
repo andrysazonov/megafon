cfda5d672ee86f1ad4f431e0a75ee32b9d3508e3
vatergo <vatergo@mail.ru>
2019-03-24 16:47:27 +0500

    update

--- megafon/megafon/urls.py
+++ megafon/megafon/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path(r'rss/', include('rss.urls')),
     path('admin/', admin.site.urls),
+    path(r'rss/', include('rss.urls')),
+    path('', RedirectView.as_view(url='/rss/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
